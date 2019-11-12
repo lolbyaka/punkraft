c3f74cf1bf5f70ad81e45759bd496d93a30ae297
Alexey Lobyak <crytekunitytek@gmail.com>
2019-11-12 14:25:25 +0300

    fixes;

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { PunkraftBackgroundCtx } from './utils/hooks/backgroundContext';
-import BackgroundVideo from './components/BackgroundVideo';
-import ContentContainer from './components/ContentContainer';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Pages/Home';
-import Menu from './components/Pages/Menu';
-import Events from './components/Pages/Events';
-import Contacts from './components/Pages/Contacts';
+import BackgroundVideo from './Components/BackgroundVideo';
+import ContentContainer from './Components/ContentContainer';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Pages/Home';
+import Menu from './Components/Pages/Menu';
+import Events from './Components/Pages/Events';
+import Contacts from './Components/Pages/Contacts';
 
 const App = () => {
   const [video, setVideo] = useState(true);
